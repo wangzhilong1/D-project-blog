58d952eec02d99cba55806061764ca4d0005b5d3
WIN10-907040811 <3322812013@qq.com>
2019-07-23 21:27:51 +0800

    models更改2

--- post/models.py
+++ post/models.py
@@ -1,46 +1,36 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
+from django.core.checks import Tags
 from django.db import models
 
-# Create your models here.
 
 class Category(models.Model):
-    cname=models.CharField(max_length=20,unique=True,verbose_name='类别名称')
+    cname=models.CharField(max_length=128,unique=True,verbose_name='类别名称')
 
     class Meta:
-        db_table='t_category'
-        verbose_name_plural='类别'
-
-    def __unicode__(self):
-        return u'%s'%self.cname
-
-
-
+        db_table ='t_category'
+        verbose_name_plural = '类别'
+    def __repr__(self):
+        return "%s"%self.cname
 class Tags(models.Model):
-    tname=models.CharField(max_length=20,unique=True)
+        tname = models.CharField(max_length=32,unique=True,verbose_name='标签名称')
 
-    class Meta:
-        db_table='t_tags'
-        verbose_name_plural = '标签'
-
-    def __unicode__(self):
-        return u'%s'%self.tname
-
-
+        class Meta:
+            db_table = 't_tags'
+            verbose_name_plural = '标签'
+        def __repr__(self):
+            return "%s" % self.tname
 class Post(models.Model):
-    title=models.CharField(max_length=100,unique=True)
-    desc=models.TextField()
-    content=models.RichTextField()
-    created=models.DateField(auto_now_add=True)
-    modified=models.DateTimeField(auto_now=True)
-    category=models.ForeignKey(Category)
-    tags=models.ManyToManyField(Tags)
+    title = models.CharField(max_length=128,unique=True)
+    desc = models.TextField()
+    content = models.TextField()
+    created = models.DateField(auto_now_add=True)
+    modified = models.DateField(auto_now_add=True)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tags)
 
     class Meta:
-        db_table='t_post'
+        db_table = 't_post'
         verbose_name_plural = '帖子'
-    def __unicode__(self):
-        return u'%s'%self.title
 
-
+    def __repr__(self):
+        return "%s" % self.title
