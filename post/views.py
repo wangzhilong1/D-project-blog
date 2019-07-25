69676448d34c98bbb98bed81921186df7eedef7f
WIN10-907040811 <3322812013@qq.com>
2019-07-25 16:09:32 +0800

    全局上下文处理器

--- post/views.py
+++ post/views.py
@@ -2,8 +2,11 @@ from django.core.paginator import Paginator
 from django.shortcuts import render
 from .models import Post
 # Create your views here.
+
 def index(request,pageno=1):
     postList = Post.objects.all().order_by('-created')
+    # for i in postList:
+    #     print(i.title)
     p=Paginator(postList,2)
     page = p.page(pageno)
-    return render(request,'index.html',{'page':page,'pageRange':p.page_range})
+    return render(request,'index.html',{'postlist':postList})
