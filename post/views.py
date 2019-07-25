4daaf07485a3e4d3d195bbba2fe489da7f311913
WIN10-907040811 <3322812013@qq.com>
2019-07-25 17:11:11 +0800

    修改bug

--- post/views.py
+++ post/views.py
@@ -6,4 +6,4 @@ def index(request,pageno=1):
     postList = Post.objects.all().order_by('-created')
     p=Paginator(postList,2)
     page = p.page(pageno)
-    return render(request,'index.html',{'page':page,'pageRange':p.page_range})
+    return render(request,'index.html',{'postlist':postList})
